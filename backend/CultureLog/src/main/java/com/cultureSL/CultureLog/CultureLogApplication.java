1eb107c1333c9820fd2527780a4fa37873864202
Sbadajoz127 <santiagobadajozdelgado@gmail.com>
2026-01-12 18:55:11 +0100

    Commit Inicial con SpringBoot Initialzr

---
+++ backend/CultureLog/src/main/java/com/cultureSL/CultureLog/CultureLogApplication.java
@@ -0,0 +1,13 @@
+package com.cultureSL.CultureLog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CultureLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CultureLogApplication.class, args);
+	}
+
+}
---
+++ backend/CultureLog/src/test/java/com/cultureSL/CultureLog/CultureLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cultureSL.CultureLog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CultureLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
